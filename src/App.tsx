b445c1bf1ba16b242a451992628726991d47bc28
saun23831-art <saun23831@gmail.com>
2026-09-10 21:14:06 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+
+import { FaMousePointer } from 'react-icons/fa'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+<FaMousePointer />
+<h1 className='bg-amber-600 h-14 text-center text-4xl'>Hello Bangladesh</h1>
+<button className="btn btn-warning">Warning</button>
+
+    </>
+  )
+}
+
+export default App
